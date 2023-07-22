fb38d0780696a3f4991396126d9b7b17f7ae3f28
LTS2 <leewoojin982@gmail.com>
2023-07-22 20:42:52 +0900

    Project 커밋 내용입니다.

---
+++ Project/src/main/java/com/wj/spring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.wj.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("HomeController 의 index() 메소드 실행");
+        return "index"; // => index.jsp 출력
+    }
+}
